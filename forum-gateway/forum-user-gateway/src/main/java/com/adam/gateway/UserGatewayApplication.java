ba81e11c461c657b0d3b3ed8a23c1a58a1dd62c8
IceProgrammer <13706531210@163.com>
2024-12-20 23:05:20 +0800

    删除多余代码

--- forum-gateway/forum-user-gateway/src/main/java/com/adam/gateway/UserGatewayApplication.java
+++ forum-gateway/forum-user-gateway/src/main/java/com/adam/gateway/UserGatewayApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
 @SpringBootApplication
-public class UserGatewayApplication
-{
-    public static void main( String[] args ) {
-            SpringApplication.run(UserGatewayApplication.class, args);
+public class UserGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserGatewayApplication.class, args);
     }
 }
